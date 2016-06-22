3758278ede55c88b1d231c085365ee05f8a71e9e
Zero Weight <zwt15@mails.tsinghua.edu.cn>
2016-06-22 19:31:39 +0800

    finished CS

--- Cs/Cs/Form1.Designer.cs
+++ Cs/Cs/Form1.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(177, 491);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 109);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "analyze";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(619, 496);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 98);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "play";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 644);
+            this.splitter1.TabIndex = 2;
+            this.splitter1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 368;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1777, 526);
+            this.ClientSize = new System.Drawing.Size(1770, 644);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +87,10 @@
         }
 
         #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Cs/Cs/Form1.cs
+++ Cs/Cs/Form1.cs
@@ -1,20 +1,30 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-
+using System.Media;
 namespace Cs
 {
     
     public partial class Form1 : Form
     {
+        private SolidBrush background = new SolidBrush(Color.LightGreen);
+        private SolidBrush filled = new SolidBrush(Color.Yellow);
+        private SolidBrush active = new SolidBrush(Color.Red);
+        private SolidBrush blank = new SolidBrush(Color.Gray);
+        private SolidBrush going = new SolidBrush(Color.Blue);
+        private Graphics draw;
+        System.Media.SoundPlayer player = new SoundPlayer("C:/Users/Zero Weight/Documents/GitHub/Signals-System/music/school_song_single_Eb.wav");
         const int L = 100;
         const int D = 10;
+        const int xstat = 20;
+        const int ystat = 20;
+        const int width = 15;
+        const int height = 10;
+        int go = 0;
+        int row;
+        int max;
+        int min;
         int[][] map = new int[L][];
         string temp;
         string[] dictionary = new string[D];
@@ -22,13 +32,15 @@ namespace Cs
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
             Read_In();
         }
         private void Read_In()
         {
+            min = 1000;
+            max = 0;
+            row = 0;
             StreamReader fileReader = new StreamReader("C:/Users/Zero Weight/Documents/GitHub/Signals-System/notes_onekey.txt");
             for (int i=0;i<L;i++)
             {
@@ -40,7 +52,106 @@ namespace Cs
                 foreach (string str in dictionary)
                 {
                     map[i][counter] = int.Parse(str);
+                    if (map[i][counter] < min&& map[i][counter]!=0) min = map[i][counter];
+                    if (map[i][counter] > max) max = map[i][counter];
+                    counter++;
                 }
+                row++;
+            }
+            for(int i = 0; i < row; i++)
+            {
+                for(int j = 0; j < D; j++)
+                {
+                    map[i][j] -= min;
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            button1.Enabled = false;
+            button2.Enabled = true;
+            this.CreateGraphics().FillRectangle(background, xstat, ystat, width * row, height * (max - min + 1));
+            for (int i = 0; i < row; i++)
+            {
+                if (map[i][0] < 0)
+                {
+                    leave(i);
+                    continue;
+                }
+                for(int j = 0; j < D; j++)
+                {
+                    if (map[i][j] < 0) break;
+                    paint(i, map[i][j]);
+                }
+            }
+        }
+        private void leave(int x)
+        {
+            this.CreateGraphics().FillRectangle(blank, xstat + x * width, ystat, width, height*(max-min+1));
+        }
+        private void enable(int x)
+        {
+            this.CreateGraphics().FillRectangle(going, xstat + x * width, ystat, width, height * (max - min + 1));
+        }
+        private void revert(int x)
+        {
+            this.CreateGraphics().FillRectangle(background, xstat + x * width, ystat, width, height * (max - min + 1));
+        }
+        private void fire(int x,int y)
+        {
+            draw = this.CreateGraphics();
+            draw.FillRectangle(active, xstat + x * width, ystat + (max - min - y) * height, width, height);
+        }
+        private void paint(int x,int y)
+        {
+            draw = this.CreateGraphics();
+            draw.FillRectangle(filled, xstat + x * width, ystat + (max-min - y) * height, width, height);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            player.Play();
+            timer1.Enabled = true;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (go == 0)
+            {
+                enable(go);
+                for(int i = 0; i < D; i++)
+                {
+                    if (map[go][i] < 0) break;
+                    fire(go, map[go][i]);
+                }
+                go ++;
+            }
+            else if (go == row)
+            {
+                if (map[go - 1][0] < 0) leave(go - 1);
+                else revert(go - 1);
+                timer1.Enabled = false;
+                for (int i = 0; i < D; i++)
+                {
+                    if (map[go-1][i] < 0) break;
+                    paint(go-1, map[go-1][i]);
+                }
+            }
+            else
+            {
+                if (map[go - 1][0] < 0) leave(go - 1);
+                else revert(go - 1);
+                for (int i = 0; i < D; i++)
+                {
+                    if (map[go - 1][i] < 0) break;
+                    paint(go-1, map[go-1][i]);
+                }
+                enable(go);
+                for (int i = 0; i < D; i++)
+                {
+                    if (map[go][i] < 0) break;
+                    fire(go, map[go][i]);
+                }
+                go++;
             }
         }
     }
