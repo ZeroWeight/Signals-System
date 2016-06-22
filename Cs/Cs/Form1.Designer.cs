30d0cbadba8babb9ac381ace61ae49a0ffd69413
Zero Weight <zwt15@mails.tsinghua.edu.cn>
2016-06-22 17:39:47 +0800

    Data Reading In finished

--- Cs/Cs/Form1.Designer.cs
+++ Cs/Cs/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.ClientSize = new System.Drawing.Size(1777, 526);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- Cs/Cs/Form1.cs
+++ Cs/Cs/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,11 +10,38 @@ using System.Windows.Forms;
 
 namespace Cs
 {
+    
     public partial class Form1 : Form
     {
+        const int L = 100;
+        const int D = 10;
+        int[][] map = new int[L][];
+        string temp;
+        string[] dictionary = new string[D];
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Read_In();
+        }
+        private void Read_In()
+        {
+            StreamReader fileReader = new StreamReader("C:/Users/Zero Weight/Documents/GitHub/Signals-System/notes_onekey.txt");
+            for (int i=0;i<L;i++)
+            {
+                map[i] = new int[D];
+                temp = fileReader.ReadLine();
+                if (temp == null) break;
+                dictionary = temp.Split(' ');
+                int counter = 0;
+                foreach (string str in dictionary)
+                {
+                    map[i][counter] = int.Parse(str);
+                }
+            }
         }
     }
 }
